61c141151fee75a207eae67b93ff0d75e815ea01
dunemevdata <dunemevdata@proton.me>
2025-04-25 20:16:35 -0400

    Update count_cowswap_batch.py

--- count_cowswap_batch.py
+++ count_cowswap_batch.py
@@ -7,6 +7,7 @@ df = pd.read_csv("cowswap_batch.csv.zip", compression='zip')
 
 batch_count = 0
 batch_without_cycle_count = 0
+token_counts = []  # to store token count for each batch
 
 for txn_hash, txn_hash_df in df.groupby("tx_hash"):
     batch_count += 1
@@ -24,7 +25,24 @@ for txn_hash, txn_hash_df in df.groupby("tx_hash"):
     if len(cycles) == 0:
         batch_without_cycle_count += 1
 
+    # count unique tokens in the batch
+    unique_tokens = set(txn_hash_df['sell_token']).union(txn_hash_df['buy_token'])
+    token_counts.append(len(unique_tokens))
 
+
+# Batch stats
 print(f"Total batches: {batch_count}")
 print(f"Total batches without cycles: {batch_without_cycle_count}")
 print(f"Percentage of batches without cycles: {batch_without_cycle_count / batch_count * 100:.1f}%")
+
+
+# Token count stats
+def percentage_below_threshold(threshold):
+    count = sum(1 for t in token_counts if t <= threshold)
+    return count / len(token_counts) * 100
+
+
+print(len(token_counts))
+for threshold in [2, 3, 4, 5, 6, 7, 8, 9]:
+    pct = percentage_below_threshold(threshold)
+    print(f"{pct:.1f}% of the batches have ≤ {threshold} tokens")
